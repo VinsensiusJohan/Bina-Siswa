a90438a9b55c30f6eb82f956f0f58e92fb006541
Johan <bangjev190@gmail.com>
2024-12-19 12:35:27 +0700

    pembaharuan I/O database

--- app.js
+++ app.js
@@ -21,19 +21,46 @@ const db = getDatabase();
 let username = document.getElementById("username");
 let password = document.getElementById("password");
 let role = document.getElementById("role");
+let reg_role = document.getElementById("choose_role");
+
+//Siswa
+let reg_username = document.getElementById("reg_username");
+let reg_password = document.getElementById("reg_password");
+let reg_email = document.getElementById("reg_email");
+let reg_sekolah = document.getElementById("reg_sekolah");
+let reg_gender = document.getElementById("reg_gender");
+
+//Guru
+let reg_username_guru = document.getElementById("reg_username_guru");
+let reg_password_guru = document.getElementById("reg_password_guru");
+let reg_email_guru = document.getElementById("reg_email_guru");
+let reg_ins_email = document.getElementById("reg_email_ins");
+let reg_studi = document.getElementById("reg_studi");
+let reg_experience = document.getElementById("reg_experience");
+let reg_gender_guru = document.getElementById("reg_gender_guru");
 
 // Get ID Btn
 let login_btn = document.getElementById("login");
 let register_btn = document.getElementById("register");
 let register_btn_page = document.getElementById("register_page");
 let logout_btn = document.getElementById("logout");
+let back_btn = document.getElementById("back");
 
-// Check if need 
+// Check if needed
 if(register_btn){register_btn.addEventListener('click',Add_User);}
 if(login_btn){login_btn.addEventListener('click',Check_Login);}
 if(logout_btn){logout_btn.addEventListener('click',Logout);}
+if(register_btn_page){register_btn_page.addEventListener('click',toRegisterPage);}
+if(back_btn){back_btn.addEventListener('click',toIndexPage);}
+
 
 //Defined function
+
+//Logout
+function Logout(){
+    sessionStorage.clear();
+    window.location.href = "index.html";
+}
 
 //Sweet Alert 
 function showAlert(type, title, text) {
@@ -44,22 +71,57 @@ function showAlert(type, title, text) {
     });
 }
 
+//Register page
+function toRegisterPage(){
+    window.location.href = "register.html";
+}
+
+function toIndexPage(){
+    window.location.href = "index.html";
+}
+
 //Register
 function Add_User() {
-    if (!username.value || !password.value || role.value === "") {
-        showAlert('error', 'Error', 'Semua harus diisi!');
-        return; 
-    }else{
-        set(ref(db, 'User/' + username.value), {
-            password: password.value,
-            role: role.value
+    if(reg_role.value === "siswa"){
+        if (!reg_username.value || !reg_password.value || !reg_email.value || !reg_sekolah.value || reg_gender.value === "") {
+            showAlert('error', 'Error', 'Semua harus diisi!');
+            return; 
+        }else{
+            set(ref(db, 'Siswa/' + reg_username.value), {
+                password: reg_password.value,
+                email: reg_email.value,
+                sekolah: reg_sekolah.value,
+                gender: reg_gender.value
+            }).then(() => {
+                showAlert('success', 'Berhasil!', 'Akun berhasil dibuat!');
+            }).catch((error) => {
+                console.log(error);
+                showAlert('error', 'Oops...', 'Terjadi kesalahan saat membuat akun: ' + error);
+            });
+        }
+        
+    }else if(reg_role.value === "guru"){
+        if (!reg_username_guru.value || !reg_password_guru.value || !reg_email_guru.value || !reg_ins_email.value || !reg_studi.value || !reg_experience.value || reg_gender_guru.value === "") {
+            showAlert('error', 'Error', 'Semua harus diisi!');
+            return;
+        }
+        else{
+            set(ref(db, 'Guru/' + reg_username_guru.value), {
+            password: reg_password_guru.value,
+            email_pribadi: reg_email_guru.value,
+            email_institusi: reg_ins_email.value,
+            studi: reg_studi.value,
+            exp: reg_experience.value,
+            gender: reg_gender_guru.value
         }).then(() => {
-            console.log("Success !");
             showAlert('success', 'Berhasil!', 'Akun berhasil dibuat!');
         }).catch((error) => {
             console.log(error);
             showAlert('error', 'Oops...', 'Terjadi kesalahan saat membuat akun: ' + error);
-        });
+        });}
+        
+    }else{
+        showAlert('error', 'Oops...', 'Ada sedikit masalah !');
     }
 }
 
@@ -70,33 +132,47 @@ function Check_Login() {
     if (!username.value || !password.value || role.value === "") {
         showAlert('error', 'Error', 'Semua harus diisi!');
         return;
+    }else if(role.value === "siswa"){
+        get(child(dbRef, 'Siswa/' + username.value)).then((snapshot) => {
+            if (snapshot.exists()) {
+                let password_db = snapshot.val().password;
+                if (password.value === password_db) {
+                    showAlert('success', 'Berhasil!', 'Login berhasil!');
+                    sessionStorage.setItem("username", username.value);
+                    window.location.href = "siswa.html";
+                } else {
+                    showAlert('error', 'Oops...', 'Ada salah nih!');
+                    console.log("Password tidak cocok");
+                }
+            } else {
+                showAlert('error', 'Error', 'Username tidak ditemukan!\nSilahkan register terlebih dahulu !');
+            }
+        }).catch((error) => {
+            showAlert('error', 'Error', 'Gagal terhubung ke database: ' + error.message);
+            console.error("Database error:", error);
+        });
+    }else if(role.value === "guru"){
+        get(child(dbRef, 'Guru/' + username.value)).then((snapshot) => {
+            if (snapshot.exists()) {
+                let password_db = snapshot.val().password;
+                if (password.value === password_db) {
+                    showAlert('success', 'Berhasil!', 'Login berhasil!');
+                    sessionStorage.setItem("username", username.value);
+                    window.location.href = "guru.html";
+                } else {
+                    showAlert('error', 'Oops...', 'Ada salah nih!');
+                    console.log("Password tidak cocok");
+                }
+            } else {
+                showAlert('error', 'Error', 'Username tidak ditemukan!\nSilahkan register terlebih dahulu !');
+            }
+        }).catch((error) => {
+            showAlert('error', 'Error', 'Gagal terhubung ke database: ' + error.message);
+            console.error("Database error:", error);
+        });
+    }else{
+        showAlert('error', 'Error', 'Ada yang salah nih !');
     }
 
-    get(child(dbRef, 'User/' + username.value)).then((snapshot) => {
-        if (snapshot.exists()) {
-            let password_db = snapshot.val().password;
-            let role_db = snapshot.val().role;
-            if (password.value === password_db && role.value === role_db) {
-                showAlert('success', 'Berhasil!', 'Login berhasil!');
-                sessionStorage.setItem("username", username.value);
-                if(role_db === "guru") {window.location.href = 'guru.html';}
-                else if(role_db === "siswa") {window.location.href = 'siswa.html';}
-                else{console.log("Something wrong to next page !");}
-            } else {
-                showAlert('error', 'Oops...', 'Ada salah nih!');
-                console.log("Password tidak cocok");
-            }
-        } else {
-            showAlert('error', 'Error', 'Username tidak ditemukan!\nSilahkan register terlebih dahulu !');
-        }
-    }).catch((error) => {
-        showAlert('error', 'Error', 'Gagal terhubung ke database: ' + error.message);
-        console.error("Database error:", error);
-    });
-}
-
-//Logout
-function Logout(){
-    sessionStorage.clear();
-    window.location.href = "index.html";
+    
 }
